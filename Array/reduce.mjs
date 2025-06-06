e5c285805127144586fcf7ef94975de8ab376892
Alish-Shakya <alishshakya44@gmail.com>
2025-06-06 18:48:23 +0545

    reduce

---
+++ Array/reduce.mjs
@@ -0,0 +1,8 @@
+//reduce: reduce((previous, current)=>{},0)
+// 0 is the initial value
+// let list = [1, 2, 3, 4];
+
+// let output = list.reduce((previous, current) => {
+//   return previous + current;
+// }, 10);
+// console.log(output);
--- Array/split.mjs
+++ Array/split.mjs
@@ -28,4 +28,21 @@
 // });
 // console.log(output);
 
-let input = [1, 2, 3, "a", "b", "c", 0, null, undefined, true, false];
+// let input = [1, 2, 3, "a", "b", "c", 0, null, undefined, true, false];
+
+// console.log(input.filter(Boolean));
+
+// map = [1,2,3] = [2,4,5] = returned modified value
+// filter = [1,2,3] = [2,3] = returned only true or false
+
+// find = find((value,index) =>{})
+// find only return the first matched value
+let list = [1, 20, 3, 44, 4];
+let output = list.find((value, index) => {
+  if (value > 20) {
+    return true;
+  } else {
+    return false;
+  }
+});
+console.log(output);
