89f3f3102f72a23c5b16073035633199e0106ec5
Alish-Shakya <alishshakya44@gmail.com>
2025-06-06 18:30:45 +0545

    filter

--- Array/split.mjs
+++ Array/split.mjs
@@ -7,3 +7,25 @@
 // console.log(name.join(" "));
 
 // filter => filter((value, index) =>{})
+// let list = [1, 5, 7, 8, 2];
+// let output = list.filter((value, index) => {
+//   if (value > 5) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(output);
+
+// let names = ["ram", "ari", "shyam"];
+
+// let output = names.filter((value, index) => {
+//   if (value.length > 4) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(output);
+
+let input = [1, 2, 3, "a", "b", "c", 0, null, undefined, true, false];
