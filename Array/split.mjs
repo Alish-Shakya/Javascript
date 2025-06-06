31fc7ec57192328d51240867b35f11b0476f0413
Alish-Shakya <alishshakya44@gmail.com>
2025-06-06 18:14:29 +0545

    split and join

---
+++ Array/split.mjs
@@ -0,0 +1,9 @@
+// //split and join
+// let name = "alishshakyashrestha";
+
+// console.log(name.split(" "));
+
+// let name = ["alish", "shakya", "shrestha"];
+// console.log(name.join(" "));
+
+// filter => filter((value, index) =>{})
